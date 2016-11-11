43e17e3082d3b743eace751445a04b52110a093e
John <john@olivetoast.com>
2016-11-11 12:51:49 +0000

    Initial Commit

---
+++ SyncthingBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SyncthingBar
+//
+//  Created by John on 11/11/2016.
+//  Copyright © 2016 Olive Toast Software Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
