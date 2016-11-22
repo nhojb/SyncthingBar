4df1c7d337232d56b551d8d049d5783e826db173
John <john@olivetoast.com>
2016-11-22 14:47:26 +0000

    Fixing webview/preference window keyboard shortcuts by adding main menu back into MainMenu.xib. Even though the menu is not visible, it still captures key events :-)

--- SyncthingBar/WebViewWindowController.swift
+++ SyncthingBar/WebViewWindowController.swift
@@ -12,38 +12,6 @@ import WebKit
 class WebViewWindowController : NSWindowController, WebResourceLoadDelegate {
     @IBOutlet var webView : WebView?
 
-    // NSResponder
-    override func keyDown(with event: NSEvent) {
-        if !self.performKeyEquivalent(with: event) {
-            super.keyDown(with: event)
-        }
-    }
-
-    override func performKeyEquivalent(with event: NSEvent) -> Bool {
-        // Filter out device independent flags so we can check for .command only.
-        guard event.modifierFlags.intersection(.deviceIndependentFlagsMask) == .command else {
-            return false
-        }
-
-        guard let chars = event.charactersIgnoringModifiers else {
-            return false
-        }
-
-        switch chars {
-        case "q":
-            NSApplication.shared().terminate(self)
-            return true
-        case "w":
-            self.window?.performClose(self)
-            return true
-        case "m":
-            self.window?.miniaturize(self)
-            return true
-        default:
-            return false
-        }
-    }
-
     // WebResourceLoadDelegate
     func webView(_ sender: WebView!, resource identifier: Any!, didFinishLoadingFrom dataSource: WebDataSource!) {
         if let pageTitle = dataSource.pageTitle {
